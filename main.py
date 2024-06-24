5b4ddd55300cd38592dc57b7b6afb57c0b401ab3
Kyle Gaffney <kylebryangaffney@gmail.com>
2024-06-24 09:17:24 -1000

    Update Obstacle Detection

--- main.py
+++ main.py
@@ -4,19 +4,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from PIL import Image
 
-def detect_obstacle(dino_box, background_color):
-    collision_box = {
-        'left': dino_box.left + dino_box.width,
-        'top': dino_box.top,
-        'width': 50,
-        'height': dino_box.height
-    }
-    for x in range(collision_box['left'], collision_box['left'] + collision_box['width']):
-        for y in range(collision_box['top'], collision_box['top'] + collision_box['height']):
-            if not pyautogui.pixelMatchesColor(x, y, background_color):
-                return True
-    return False
-
 GAME_URL = "https://trex-runner.com/#google_vignette"
 chrome_driver_path = "ChromeDriver\\chromedriver.exe"
 
@@ -31,7 +18,7 @@ time.sleep(2)
 
 # Load the background color from the provided image
 bg_image = Image.open("bgColor.png")
-background_color = bg_image.getpixel((20, 6))[:3]
+background_color = bg_image.getpixel((20, 20))[:3]
 print(f"Background color: {background_color}")
 
 time.sleep(2)
@@ -41,21 +28,34 @@ pyautogui.keyUp('space')
 
 time.sleep(2)
 
-# Locate the dinosaur on screen within a specific region
-dino_location = pyautogui.locateOnScreen("dino.png")
-if dino_location:
-    print(f"Dino located at: {dino_location}")
-    ## Dino located at: Box(left=np.int64(548), top=np.int64(778), width=51, height=57)
+# Locate the dinosaur on screen
+dino_box = pyautogui.locateOnScreen("dino.png", confidence=0.8)
+if dino_box:
+    print(f"Dino located at: {dino_box}")
 else:
-    print("Dino not found on screen.")
     driver.quit()
     exit()
 
+def detect_obstacle(dino_box, background_color):
+    collision_box = {
+        'left': dino_box.left + dino_box.width,
+        'top': dino_box.top - (dino_box.top // 2),
+        'width': 125,
+        'height': dino_box.height // 2
+    }
+    for x in range(collision_box['left'], collision_box['left'] + collision_box['width']):
+        for y in range(collision_box['top'], collision_box['top'] + collision_box['height']):
+            if not pyautogui.pixelMatchesColor(x, y, background_color):
+                print(f"Obstacle at: {x, y}")
+                return True
+    return False
+
 while True:
-    dino_location = pyautogui.locateOnScreen("dino.png")
-    if dino_location and detect_obstacle(dino_location, background_color):
+    dino_box = pyautogui.locateOnScreen("dino.png", confidence=0.8)
+    if dino_box and detect_obstacle(dino_box, background_color):
         pyautogui.keyDown('space')
         pyautogui.keyUp('space')
+        time.sleep(0.2)
     time.sleep(0.05)
 
 driver.quit()
